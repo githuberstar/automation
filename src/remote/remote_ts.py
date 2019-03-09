b9f7b7354f5dfca6208a0e137dab2e524d12ecf7
zhujh <zhujihui1991@gmail.com>
2019-03-09 23:43:02 +0800

    11

--- src/remote/remote_ts.py
+++ src/remote/remote_ts.py
@@ -3,7 +3,7 @@ from selenium.webdriver import Remote
 lists = {'http://192.168.19.102:5555/wd/hub': 'chrome'}
 lists2 = {'http://127.0.0.1:4444/wd/hub': 'chrome'}
 
-for host, browser in lists2.items():
+for host, browser in lists.items():
     print(host, browser)
     driver = Remote(command_executor=host,
                     desired_capabilities={'platform': 'ANY',
